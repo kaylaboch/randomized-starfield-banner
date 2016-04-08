f5c9f587352e1f333ea8755d50897606ffbc8747
kaylaboch <boch.kayla57@gmail.com>
2016-04-07 20:05:13 -0400

    second set of vars

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ var $banner = $('.banner');
 var $width= $banner.outerWidth();
 var $addStars;
 var $heigh = $banner.outerHeight();
+
+var $body = $('.banner');
+var width = $body.outerWidth();
+var $star;
+var height = $body.outerHeight();
