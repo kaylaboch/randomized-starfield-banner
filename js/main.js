9600a9eeb3bbc9867feecd406e23a230b250979d
kaylaboch <boch.kayla57@gmail.com>
2016-04-07 20:15:54 -0400

    spacing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var $banner = $('.banner');
-var $width= $banner.outerWidth();
+var $width = $banner.outerWidth();
 var $addStars;
 var $height = $banner.outerHeight();
 
@@ -15,7 +15,7 @@ for (i = 0; i < 75; i++) {
     top: Math.random() * height,
     left: Math.random() * width,
     opacity: Math.random(),
-    transform: 'rotate(' + Math.random() *360 +'deg) scale(' + Math.random() + ')'
+    transform: 'rotate(' + Math.random() *360 + 'deg) scale(' + Math.random() + ')'
   });
 
   $body.append($star);
