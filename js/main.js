170f1fbcfd0960e6ee01dc2152ea8955edd9201c
kaylaboch <boch.kayla57@gmail.com>
2016-04-07 20:10:02 -0400

    added more and fixed things

--- js/main.js
+++ js/main.js
@@ -9,7 +9,14 @@ var $star;
 var height = $body.outerHeight();
 
 for (i = 0; i < 75; i++) {
-  $star = $('div');
+  $star = $('<div>');
   $star.addClass('star');
-  $star.css()
+  $star.css({
+    top: Math.random() * height,
+    left: Math.random() * width,
+    opacity: Math.random(),
+    transform: 'rotate(' + Math.random() *360 +'deg) scale(' + Math.random() + ')'
+  });
+
+  $body.append($star);
 }
