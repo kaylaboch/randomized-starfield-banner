cfe4fb4ae3a1cf50396184482d2019842d0de475
kaylaboch <boch.kayla57@gmail.com>
2016-04-07 20:06:54 -0400

    adding class

--- js/main.js
+++ js/main.js
@@ -7,3 +7,9 @@ var $body = $('.banner');
 var width = $body.outerWidth();
 var $star;
 var height = $body.outerHeight();
+
+for (i = 0; i < 75; i++) {
+  $star = $('div');
+  $star.addClass('star');
+  $star.css()
+}
